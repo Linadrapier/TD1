993606a057b87bd188f0c2f4af8c01950978fd67
Linadrapier <117764282+Linadrapier@users.noreply.github.com>
2022-11-10 14:54:08 +0100

    commit test

--- src/histoire/Monhistoire.java
+++ src/histoire/Monhistoire.java
@@ -4,7 +4,8 @@ public class Monhistoire {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+String s = "hello";
+System.out.println(s);
 	}
 
 }
--- src/personnages/humain.java
+++ src/personnages/humain.java
@@ -1,5 +1,61 @@
 package personnages;
 
+
 public class humain {
+	private String nom;
+	private int argent;
+	private String boisson;
+	
+	public humain(string nom, int argent, string boisson) {
+		this.nom = nom;
+		this.argent = argent;
+		this.boisson = boisson;
+	}
+	
+	public void parler(string texte) {
+		System.out.println("("+this.nom+")-"+texte);
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour! Je m'appelle "+this.nom+" et j'aime boire du "+this.boisson+"et j'ai "+this.argent+" sous en poche"); 
+	}
+	
+	public void boire() {
+		parler("hum, un bon verre de "+this.boisson" !GLOUPS!");
+		
+	}
+
+	public string getNom() {
+		return nom;
+	}
+
+	public void setNom(string nom) {
+		this.nom = nom;
+	}
+
+	public int getArgent() {
+		return argent;
+	}
+
+	public void setArgent(int argent) {
+		this.argent = argent;
+	}
+
+	public string getBoisson() {
+		return boisson;
+	}
+
+	public void setBoisson(string boisson) {
+		this.boisson = boisson;
+	}
+	
+	void gagnerArgent(int n) {
+		this.argent+=n;
+					
+	}
+	void perdreArgent(int n) {
+		this.argent-=n;
+					
+	}
 
 }
