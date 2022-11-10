5c206ae0c7e856cba297777bc4c4bd2c304ba17b
Lina <lina.drapier@gmail.com>
2022-11-10 14:19:51 +0100

    fix repo

---
+++ src/histoire/Monhistoire.java
@@ -0,0 +1,10 @@
+package histoire;
+
+public class Monhistoire {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author linad
+ *
+ */
+module TD1 {
+}
